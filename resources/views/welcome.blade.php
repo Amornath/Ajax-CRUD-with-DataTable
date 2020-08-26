16f844b2b177a0958097b6e7ef5d3eb4a0057c20
Amornath <amor2017nstucste@gmail.com>
2020-08-26 02:01:00 -0700

    Initial Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>CRUD APPLICATION</title>
+</head>
+<body>
+<h3 style="    display: flex;
+   align-items: center; 
+   justify-content: center; padding-top: 150px;">Type url localhost/crud/ajaxproducts</h3>
+</body>
+</html>
